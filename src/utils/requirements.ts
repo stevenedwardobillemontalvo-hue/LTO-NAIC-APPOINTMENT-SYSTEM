9863e7e8e72aa25207d35316543aaaf624bece59
Steven <smontalvo@tritonica.io>
2026-02-11 12:38:44 +0800

    .

--- src/utils/requirements.ts
+++ src/utils/requirements.ts
@@ -1,26 +1,21 @@
-export const transactionRequirements: Record<
-  string,
-  { key: string; label: string }[]
-> = {
+export const transactionRequirements: Record<string, string[]> = {
   "Non-Professional Driver License Application": [
-    { key: "apl_form", label: "Application for Permits and License (APL) Form" },
-    { key: "medical_certificate", label: "Medical Certificate" },
-    { key: "pdc", label: "Practical Driving Course (PDC)" },
-    { key: "student_permit", label: "Valid Student Permit" },
-    { key: "psa_birth_certificate", label: "PSA Birth Certificate" },
+    "Application for Permits and License (APL) Form",
+    "Medical Certificate",
+    "Practical Driving Course (PDC)",
+    "Valid Student Permit",
+    "PSA Birth Certificate",
   ],
-
   "Professional Driver License Application": [
-    { key: "apl_form", label: "Application for Permits and License (APL) Form" },
-    { key: "medical_certificate", label: "Medical Certificate" },
-    { key: "pdc", label: "Practical Driving Course (PDC)" },
-    { key: "non_pro_license", label: "Valid Non-Professional License" },
-    { key: "psa_birth_certificate", label: "PSA Birth Certificate" },
+    "Application for Permits and License (APL) Form",
+    "Medical Certificate",
+    "Practical Driving Course (PDC)",
+    "Valid Non-Professional License",
+    "PSA Birth Certificate",
   ],
-
   "Adding Restriction": [
-    { key: "apl_form", label: "Application for Permits and License (APL) Form" },
-    { key: "medical_certificate", label: "Medical Certificate" },
-    { key: "drivers_license", label: "Valid Driver's License" },
+    "Application for Permits and License (APL) Form",
+    "Medical Certificate",
+    "Valid Driver's License",
   ],
 };
